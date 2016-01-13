c64d596998445c84b11c6f13e29381c37f728c6d
Judy Mou <moujudy@gmail.com>
2016-01-13 11:51:34 -0800

    reset config

--- fund.js
+++ fund.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var path = require('path');
 var app = express();
 
-//app.use(bodyParser());
+app.use(bodyParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
